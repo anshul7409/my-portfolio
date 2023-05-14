b0d8d04357eee97435923ecc918a650b9526de79
Anshul Rawat <79444489+anshul7409@users.noreply.github.com>
2023-05-14 20:41:27 +0530

    added contact and footer sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,11 +55,47 @@ var swiperProjects = new Swiper(".projects__container", {
 });
 
 
-/*=============== SWIPER TESTIMONIAL ===============*/
-
-
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactName = document.getElementById('contact-name'),
+      contactEmail = document.getElementById('contact-email'),
+      contactProject = document.getElementById('contact-project'),
+      contactMessage = document.getElementById('contact-message');
 
+const sendEmail = (e) => { 
+  e.preventDefault()
+
+  // Check if the field has a value
+  if(contactName.value === ''  ||  contactEmail.value === '' || contactProject.value === ''){
+       // Add and remove color
+        contactMessage.classList.remove('color-blue');
+        contactMessage.classList.add('color-red');
+        // Show message	
+        contactMessage.textContent = 'Write all the input fields 📩';
+  }else{
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_e005txe','template_xm8dqmy','#contact-form','gdd_fy7sCazDWORcW')
+            .then(() => {
+                // Show message and add color
+                contactMessage.classList.remove('color-blue')
+                contactMessage.textContent = 'Message sent successfully ✔️';
+
+                // Remove message after five seconds
+                setTimeout(() => {
+                    contactMessage.textContent = ''
+                },5000)
+            },(error) =>{
+                alert('OOPS! Something went wrong..',error);
+            })
+
+            // To clear the input field
+            contactName.value = '';
+            contactEmail.value = '';
+            contactProject.value = '';
+    }
+}
+
+contactForm.addEventListener('submit', sendEmail);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
